ae927f8f2901c1d41445eb401dc70ee2a103654f
Premapriya1905 <Premapriya1905@gmail.com>
2024-03-18 11:02:32 +0530

    alldone

--- frontend/src/Component/UpdateUser.jsx
+++ frontend/src/Component/UpdateUser.jsx
@@ -32,7 +32,7 @@ function UpdateUser () {
             COOKINGTIME:cookingtime,
             NUTRITIONALINFORMATION:nutritionalinformation,
             CREATEDBY:createdby
-    })
+    },{headers:{authorization:`Bearer ${token}`}})
     .then((response)=>{ console.log(response.data);
     navigate('/')})
     .catch((error)=> console.error(error))
@@ -43,27 +43,27 @@ function UpdateUser () {
             <div>
                 <form onSubmit={Formsubmit}>
                     <h2 className="add-user">Update recipe</h2>
-                    <div className="div8">
+                    <div className="div1">
                         <label className="create1">Id</label>
                         <input type="text" defaultValue={id} placeholder="Enter Id" className="create2" onChange={(e)=>setId(e.target.value)}/>
                     </div>
-                    <div className="div1">
+                    <div className="div2">
                         <label className="create1">Recipe</label>
                         <input type="text" defaultValue={recipe} placeholder="Enter Recipe" className="create2" onChange={(e)=>setRecipe(e.target.value)}/>
                     </div>
-                    <div className="div2">
+                    <div className="div3">
                         <label className="create1">Instructions</label>
                         <input type="text" defaultValue={instructions} placeholder="Enter Instructions" className="create2" onChange={(e)=>setInstructions(e.target.value)}/>
                     </div>
-                    <div className="div3">
+                    <div className="div4">
                         <label className="create1">Cookingtime</label>
                         <input type="text" defaultValue={cookingtime} placeholder="Enter Cookingtime" className="create2" onChange={(e)=>setCookingTime(e.target.value)}/>
                     </div>
-                    <div className="div4">
+                    <div className="div5">
                         <label className="create1">Nutritionalinformation</label>
                         <input type="text" defaultValue={nutritionalinformation} placeholder="Enter Nutritinalinformation" className="create2" onChange={(e)=>setNutritionalInformation(e.target.value)}/>
                     </div>
-                    <div className="div5">
+                    <div className="div6">
                         <label className="create1">Createdby</label>
                         <input type="text" defaultValue={createdby} placeholder="Enter Createdby" className="create2" onChange={(e)=>setCreatedBy(e.target.value)}/>
                     </div>
